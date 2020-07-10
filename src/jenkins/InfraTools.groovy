530118266eecd489013fc5d204442bbe336bdc5f
vinayak <vinayak.prasannakumar@mindcurv.com>
2020-07-10 18:06:47 +0530

    removed unwanted

--- src/jenkins/InfraTools.groovy
+++ src/jenkins/InfraTools.groovy
@@ -20,42 +20,7 @@ def parameter(){
         $class: 'JiraProjectProperty'
     ], 
     
-    parameters([
-        
-        [
-            
-            $class: 'ExtensibleChoiceParameterDefinition', 
-            choiceListProvider: [$class: 'TextareaChoiceListProvider',
-            addEditedValue: false,
-            choiceListText: '''DoNothing\nEnable\nDisable''',
-            defaultChoice: 'DoNothing'],
-            description: '',
-            editable: false,
-            name: 'JobAction'
-            
-        ],
-        
-        [
-            
-            $class: 'CascadeChoiceParameter',
-            choiceType: 'PT_CHECKBOX', 
-            description: '', 
-            filterLength: 1, 
-            filterable: false, 
-            name: 'JobName', 
-            randomName: 'choice-parameter-38182946397479', 
-            referencedParameters: 'JobAction', 
-            script: [
-                $class: 'GroovyScript', 
-                fallbackScript: [classpath: [], 
-                sandbox: false, 
-                script: ''], 
-                script: [classpath: [], 
-                sandbox: false, 
-                script: '''if (JobAction.equals("Enable") || JobAction.equals("Disable")) {\nreturn ["NF_PRO_Indexing_Automation","NF_PRO_Re-Indexing","F_PROD_Re-Indexing","StoreFront_Restart_On_AjpRejectedCount","Food_StoreFront_Restart_On_AjpRejectedCount"]\n}''']
-                ]
-                
-        ],
+   
         
         [
             $class: 'ExtensibleChoiceParameterDefinition',
@@ -88,25 +53,7 @@ def parameter(){
             ]
         ],
          
-        [
-            $class: 'CascadeChoiceParameter', 
-            choiceType: 'PT_CHECKBOX', 
-            description: '', 
-            filterLength: 1, 
-            filterable: false, 
-            name: 'Call_automation_Non_food', 
-            randomName: 'choice-parameter-38182959006666', 
-            referencedParameters: 'EventHandlerAction', 
-            script: [
-                $class: 'GroovyScript', 
-                fallbackScript: [classpath: [], 
-                sandbox: false, 
-                script: ''], 
-                script: [classpath: [], 
-                sandbox: false, 
-                script: '''if (EventHandlerAction.equals("Enable") || EventHandlerAction.equals("Disable")) {\nreturn ["check_pod_restartcount_nonbusiness_ms_esb_product_provider","check_atuin_CC_Confirmed_ordercount","check_atuin_CC_Confirmed_ordercount_WeekEnd","check_atuin_CC_Integrated_ordercount","check_atuin_CC_Issued_ordercount","check_atuin_CC_Preparing_ordercount","check_atuin_CC_Reserved_ordercount","check_atuin_Textile_confirmed_ordercount","check_atuin_Textile_confirmed_ordercount_WeekEnd","check_atuin_Textile_issued_ordercount","check_atuin_Textile_preparing_ordercount","check_atuin_Textile_reserved_ordercount","check_atuin_almacen_cancelled_ordercount","check_atuin_almacen_error_ordercount","check_atuin_almacen_integrated_ordercount","check_atuin_almacen_invoiced_ordercount","check_atuin_almacen_issued_ordercount","check_atuin_almacen_preparing_ordercount","check_atuin_almacen_reserved_ordercount","check_atuin_orders_cancelled_after_checkout","check_atuin_textile_integrated_ordercount","atuin-prod-order-status-update_lag","atuin-prod-payment-status-update_lag","atuin-prod-product-volumetry_lag"]\n}''']
-            ]
-        ], 
+
         
         [
             $class: 'CascadeChoiceParameter', 
@@ -137,7 +84,7 @@ def parameter(){
             filterable: false, 
             name: 'EventHandler', 
             randomName: 'choice-parameter-38182962295796', 
-            referencedParameters: 'ServiceName,Call_automation_Non_food,C4Environment', 
+            referencedParameters: 'ServiceName,C4Environment', 
             script: [
                 $class: 'GroovyScript', 
                 fallbackScript: [classpath: [], 
@@ -148,26 +95,9 @@ def parameter(){
                 script: 'return ["sf_restart","ATUIN_OrderCount_Escalations","check_ds_session","mdex_load_eventhandler"]']
                 
                 ]
-        ],
-        
-        [
-            $class: 'CascadeChoiceParameter', 
-            choiceType: 'PT_RADIO', 
-            description: '', filterLength: 1, 
-            filterable: false, 
-            name: 'BotsExcluded', 
-            randomName: 'choice-parameter-38182989621050', 
-            referencedParameters: 'ServiceName', 
-            script: [
-                $class: 'GroovyScript', 
-                fallbackScript: [classpath: [], 
-                sandbox: false, 
-                script: ''], 
-                script: [classpath: [], 
-                sandbox: false, 
-                script: 'return ["YES:selected"]']
-                ]
         ]
+        
+
     ])
 ])
 }
